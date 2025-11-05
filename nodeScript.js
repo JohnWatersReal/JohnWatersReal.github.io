6c69f588d1812b923c842d1abbf0e6c4b5dc1972
John Waters <jp.waters@hotmail.com>
2025-11-06 00:16:35 +1100

    making progress oh yeah

--- nodeScript.js
+++ nodeScript.js
@@ -3,12 +3,12 @@ const dir = './Crosswords/Collection 1/Standard';
 
 fs.readdir(dir, (err, data) => {
     console.log(data.length);
+    var standardNames = [];
+    for (var i = 0; i < data.length; i++) {
+        standardNames[i] = data[i];
+    fs.writeFile("output.json", standardNames);
+}
 });
 
-var standardNames = [];
-for (var i = 0; i < data.length; i++) {
-    standardNames[i] = data[i];
-}
 
-fs.writeFile("output.json", standardNames);
-//AHHH
+
