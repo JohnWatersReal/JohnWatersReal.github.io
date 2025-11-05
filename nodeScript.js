ec058fa8a362453bda1abf71c37369e9ac6782f9
John Waters <jp.waters@hotmail.com>
2025-11-05 23:50:49 +1100

    node is going to break this isnt going to work :(

---
+++ nodeScript.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const dir = './Crosswords/Collection 1/Standard';
+
+fs.readdir(dir, (err, data) => {
+    console.log(data.length);
+});
+
+var standardNames = [];
+for (var i = 0; i < data.length; i++) {
+    standardNames[i] = data[i];
+}
+
+fs.writeFile("output.json", standardNames);
