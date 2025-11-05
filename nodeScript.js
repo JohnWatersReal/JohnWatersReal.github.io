d3f41e25de18b4d009985cecb003cbd59bb2bd30
John Waters <jp.waters@hotmail.com>
2025-11-06 00:38:27 +1100

    looking for error

--- nodeScript.js
+++ nodeScript.js
@@ -2,12 +2,17 @@ const fs = require('fs');
 const dir = './Crosswords/Collection 1/Standard';
 
 fs.readdir(dir, (err, data) => {
+    if (err) {
+        console.error("Directory ", err);
+        process.exit(1);
+    }
     console.log(data.length);
     var standardNames = [];
     for (var i = 0; i < data.length; i++) {
         standardNames[i] = data[i];
     }
     fs.writeFileSync("output.json", JSON.stringify(standardNames));
+    console.log("Wrote " + standardNames.length + " files to output.json");
 });
 
 
